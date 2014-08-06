98e92c83ec1e40b5f37075918265104f201bdc79
nuked <F.R.M.Barnes@kent.ac.uk>
2014-08-06 12:00:46 +0100

    Assorted updates for AVR PAL testing.
    This now sort of works, as expected!  Could probably squeeze a few
    more cycles out here and there in the interrupt routines, but good
    enough for now.  Started working on some text/font stuff.

--- tests/test_avr33.asm
+++ tests/test_avr33.asm
@@ -36,29 +36,15 @@
 .equ	CYC_H_SYNC	=74		; (74.2)
 
 .equ	PAL_CYC_SCANLINE	=1023	; 64us lines
-.equ	PAL_CYC_OUTPUT_START	=178
-.equ	PAL_CYC_VGATE_START	=195	; turn on here (OC1B)
+.equ	PAL_CYC_OUTPUT_START	=83
+.equ	PAL_CYC_VGATE_START	=208	; turn on here (OC1B)
 
 ; NOTE: r4 determines vertical region (0=active, 1=bblank, 2=vsync, 3=tblank),
 ;       r5 is the particular line in that region.
 .equ	PAL_ACTIVE_LINES	=192
+.equ	PAL_BBLANK_LINES	=59
+.equ	PAL_VSYNC_LINES		=9
 .equ	PAL_TBLANK_LINES	=52
-.equ	PAL_VSYNC_LINES		=8
-.equ	PAL_BBLANK_LINES	=60
-
-;.equ	PAL_TIME_SCANLINE	=64
-;.equ	PAL_TIME_OUTPUT_START	=11
-;
-;.equ	PAL_LINE_FRAME		=312
-;.equ	PAL_LINE_START_VSYNC	=0
-;.equ	PAL_LINE_STOP_VSYNC	=7
-;.equ	PAL_LINE_DISPLAY	=260
-;.equ	PAL_LINE_MID		=156	; (((PAL_LINE_FRAME - PAL_LINE_DISPLAY) / 2) + (PAL_LINE_DISPLAY / 2))
-;
-;.equ	PAL_CYC_SCANLINE	=1023
-;.equ	PAL_CYC_COLOUR_START	=85
-;.equ	PAL_CYC_OUTPUT_START	=178	; 175
-;.equ	PAL_CYC_VGATE_START	=195	; turn on here
 
 ; video connection
 .equ	VID_PORT		=PORTD
@@ -98,280 +84,110 @@ VEC_timer1ovf:	;{{{  interrupt for TIMER1 overflow
 	push	r17				;								[5]
 
 	dec	r5				; next line please						[6]
-	brvc	5f				; straightforward: nothing special here				[7+1]
+	mov	r16, r5				;								[7]
+	cpi	r16, 0xff			; see if we rolled over						[8]
+	brne	5f				; straightforward: nothing special here				[9+1]
 	; rolled over, entering new region
-	mov	r16, r4				; current V region						[8]
-	cpi	r16, 0				;								[9]
-	breq	1f				;								[10+1]
-	cpi	r16, 1				;								[11]
-	breq	2f				;								[12+1]
-	cpi	r16, 2				;								[13]
-	breq	3f				;								[14+1]
+	mov	r16, r4				; current V region						[10]
+	cpi	r16, 0				;								[11]
+	breq	1f				;								[12+1]
+	cpi	r16, 1				;								[13]
+	breq	2f				;								[14+1]
+	cpi	r16, 2				;								[15]
+	breq	3f				;								[16+1]
 	; else must be 3 (top-blanking region), moving into active region
-	ldi	r16, PAL_ACTIVE_LINES-1		;								[15]
-	mov	r5, r16				;								[16]
-	clr	r4				;								[17]
-	rjmp	6f				;								[19]
+	ldi	r16, PAL_ACTIVE_LINES-1		;								[17]
+	mov	r5, r16				;								[18]
+	clr	r4				;								[19]
+	rjmp	6f				;								[21]
 
-.L1:	; leaving active region into bottom-blanking								-> [11]
-	ldi	r16, PAL_BBLANK_LINES-1		;								[12]
-	mov	r5, r16				;								[13]
-	inc	r4				;								[14]
-	rjmp	9f				;								[16]
-
-.L2:	; leaving bottom-blanking into vsync region								-> [13]
-	ldi	r16, PAL_VSYNC_LINES-1		;								[14]
+.L1:	; leaving active region into bottom-blanking								-> [13]
+	ldi	r16, PAL_BBLANK_LINES-1		;								[14]
 	mov	r5, r16				;								[15]
 	inc	r4				;								[16]
-	; set OC1A for VSYNC
-	ldi	r17:r16, CYC_V_SYNC		;								[17]
-	sts	OCR1AH, r17			;								[19]
-	sts	OCR1AL, r16			;								[21]
-	rjmp	9f				;								[23]
+	; better make sure we turn off OC1C here
+	ldi	r16, TIMSK1_BIT_TOIE1		;								[17]
+	sts	TIMSK1, r16			; turn off OC1C interrupt					[19]
+	ldi	r16, TIFR1_BIT_OCF1C		;								[20]
+	sts	TIFR1, r16			; clear any pending OC1C interrupt				[22]
 
-.L3:	; leaving vsync region into top-blanking								-> [15]
-	ldi	r16, PAL_TBLANK_LINES-1		;								[16]
+	rjmp	9f				;								[24]
+
+.L2:	; leaving bottom-blanking into vsync region								-> [15]
+	ldi	r16, PAL_VSYNC_LINES-1		;								[16]
 	mov	r5, r16				;								[17]
 	inc	r4				;								[18]
-	; set OC1A timing for HSYNC
-	ldi	r17:r16, CYC_H_SYNC		;								[19]
+	; set OC1A for VSYNC
+	ldi	r17:r16, CYC_V_SYNC		;								[19]
 	sts	OCR1AH, r17			;								[21]
 	sts	OCR1AL, r16			;								[23]
+
 	rjmp	9f				;								[25]
 
-.L5:						;								-> [8]
-	tst	r4				;								[9]
-	brne	9f				; branch if not in active region				[10+1]
-	nop					;								[11]
-	nop
-	nop
-	nop
-	nop					;								[15]
-	nop
-	nop
-	rjmp	7f				;								[19]
-.L6:	; first active line [191]										-> [19]
+.L3:	; leaving vsync region into top-blanking								-> [17]
+	ldi	r16, PAL_TBLANK_LINES-1		;								[18]
+	mov	r5, r16				;								[19]
+	inc	r4				;								[20]
+	; set OC1A timing for HSYNC
+	ldi	r17:r16, CYC_H_SYNC		;								[21]
+	sts	OCR1AH, r17			;								[23]
+	sts	OCR1AL, r16			;								[25]
+	rjmp	9f				;								[27]
+
+.L5:						;								-> [10]
+	tst	r4				;								[11]
+	brne	9f				; branch if not in active region				[12+1]
+	rjmp	7f				;								[14]
+
+.L6:	; first active line [191]										-> [21]
 	; turn on OC1C and its interrupt
-	ldi	YH, hi(V_framebuffer)		;								[20]
-	ldi	YL, lo(V_framebuffer)		;								[21]
-	ldi	r16, TIFR1_BIT_OCF1C		;								[22]
-	sts	TIFR1, r16			; clear any past OC1C interrupt					[24]
-	ldi	r16, (TIMSK1_BIT_OCIE1C | TIMSK1_BIT_TOIE1)
+	ldi	YH, hi(V_framebuffer)		;								[22]
+	ldi	YL, lo(V_framebuffer)		;								[23]
+	ldi	r16, TIFR1_BIT_OCF1C		;								[24]
+	sts	TIFR1, r16			; clear any past OC1C interrupt					[26]
+	ldi	r16, (TIMSK1_BIT_OCIE1C | TIMSK1_BIT_TOIE1)	;						[27]
+	sts	TIMSK1, r16			; enable OC1C and overflow interrupts				[29]
 
-	ldi	r16, TIMSK1_BIT_OCIE1C
-	lds	r16, TIMSK1
-	ori	r16, TIMSK1_BIT_OCIE1C
 
-.L7:	; in active line [191-0]										-> [19]
+.L7:	; in active line [191-0]										-> [14, 29]
 
-.L9:						;								-> [16, 23, 25, 11, 19]
+.L9:						;								-> [24, 25, 27, 13, 14, 29]
 	; here we are in the new V region (r4).
+	;								-> [24] if just moved into bottom-blanking (and OC1C now all clear)
+	;								-> [25] if just moved into vsync (and now generating vsync pulses)
+	;								-> [27] if just moved into top-blanking (and now generating hsync pulses)
+	;								-> [13] if in the middle of some non-active region, possibly last time
+	;								-> [14] if in the middle of the active region
+	;								-> [29] if just moved into active region (and OC1C now enabled, YH:YL set)
 
-	pop	r17
+	pop	r17				;								[<= 30 cycles to here].
 	pop	r16
 	out	SREG, r2			; restore SREG
 	reti
 
 ;}}}
-PR_vsync: ;{{{  rest-of-ISR for vsync lines, also initial case
-	ldi	r19:r18, DPY_VSYNC_END
-	cp	r16, r18
-	cpc	r17, r19
-	breq	1f			; branch if V_scanline == DPY_VSYNC_END
-	ldi	r19:r18, DPY_LINES_FRAME
-	cp	r16, r18
-	cpc	r17, r19
-	brlo	2f			; branch if V_scanline <= DPY_LINES_FRAME  (normal vsync line)
-	breq	2f
-	; assert: V_scanline > DPY_LINES_FRAME
-	ldi	r19:r18, CYC_V_SYNC	; setup for vsync pulses
-	sts	OCR1AH, r19
-	sts	OCR1AL, r18
-	clr	r16			; V_scanline = 0
-	clr	r17
-
-	; DEBUG updates for next frame
-	lds	r18, V_xxmask
-	inc	r18
-	sts	V_xxmask, r18
-
-	rjmp	2f
-.L1:	; assert: V_scanline == DPY_VSYNC_END
-	ldi	r19:r18, CYC_H_SYNC	; setup for hsync pulses
-	sts	OCR1AH, r19
-	sts	OCR1AL, r18
-	; next-time, in blanking portion
-	ldi	r19, hi(PR_tblank >> 1)
-	ldi	r18, lo(PR_tblank >> 1)
-	sts	A_vline_h, r19
-	sts	A_vline_l, r18
-.L2:
-	rjmp	ENDVEC_timer1ovf
-
-
-;}}}
-PR_tblank: ;{{{  rest-of-ISR for top blanking lines
-	ldi	r19:r18, DPY_START_RENDER-1
-	cp	r16, r18
-	cpc	r17, r19
-	breq	1f			; branch if V_scanline == DPY_START_RENDER-1
-	rjmp	9f
-.L1:
-	; prepare for active lines next
-	ldi	r19, hi(PR_aline >> 1)
-	ldi	r18, lo(PR_aline >> 1)
-	sts	A_vline_h, r19
-	sts	A_vline_l, r18
-
-	ldi	r19:r18, 0		; set V_roffs = 0
-	sts	V_roffs_h, r19
-	sts	V_roffs_l, r18
-
-	ldi	r18, VSCALE		; set V_vscale = VSCALE
-	sts	V_vscale, r18
-.L9:
-	rjmp	ENDVEC_timer1ovf
-
-
-;}}}
-PR_aline: ;{{{  rest-of-ISR for active lines (from DPY_START_RENDER .. DPY_STOP_RENDER)
-	ldi	r19:r18, DPY_STOP_RENDER
-	cp	r16, r18
-	cpc	r17, r19
-	breq	8f			; branch if V_scanline == DPY_STOP_RENDER  (and not rendering this line)
-
-	; okis, rendering this one.
-
-	; setup USART in SPI mode to blat data out
-	ldi	r18, HRES
-	mov	r3, r18
-
-	; setup 'Y' (r29:r28) to be address of line data start
-	lds	r29, V_roffs_h
-	ldi	r18, 4			; XXX: must match MSB of framebuffer address
-	add	r29, r18
-	lds	r28, V_roffs_l
-
-	ldi	r18, 0xc0		; MSPIM mode
-	sts	UCSR1C, r18
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	ldi	r18, 0x60		; TXC1, UDRE1
-	sts	UCSR1A, r18		; clear any pending interrupts
-	ldi	r18, 0x68		; UDRIE, TXCIE, TXEN1
-	sts	UCSR1B, r18
-	ldi	r19:r18, 2		; baud rate
-	sts	UBRR1H, r19
-	sts	UBRR1L, r18
-	; transmitter up-and-running, UDRE interrupt will be ready to fire
-	ldi	r18, 0x00
-	;ld	r18, Y+
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	sts	UDR1, r18
-
-	rjmp	9f
-.L8:
-	; not rendering anymore, go into bottom blanking
-
-	ldi	r19, hi(PR_bblank >> 1)
-	ldi	r18, lo(PR_bblank >> 1)
-	sts	A_vline_h, r19
-	sts	A_vline_l, r18
-	rjmp	99f
-.L9:
-	;{{{  update V_vscale and V_roffs
-	lds	r18, V_vscale
-	cpi	r18, 0
-	breq	93f
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	dec	r18
-	rjmp	94f
-.L93:
-	ldi	r18, VSCALE
-	lds	r25, V_roffs_h
-	lds	r24, V_roffs_l
-	adiw	r25:r24, HRES		; V_roffs += HRES
-	sts	V_roffs_h, r25
-	sts	V_roffs_l, r24
-	nop
-.L94:
-	sts	V_vscale, r18
-
-	;}}}
-.L99:
-	rjmp	ENDVEC_timer1ovf
-
-
-;}}}
-PR_bblank: ;{{{  rest-of-ISR for bottom blanking lines
-	ldi	r19:r18, DPY_LINES_FRAME
-	cp	r16, r18
-	cpc	r17, r19
-	breq	1f
-	rjmp	9f
-.L1:
-	; set to vsync for next.
-	ldi	r19, hi(PR_vsync >> 1)
-	ldi	r18, lo(PR_vsync >> 1)
-	sts	A_vline_h, r19
-	sts	A_vline_l, r18
-.L9:
-	rjmp	ENDVEC_timer1ovf
-
-
-;}}}
-ENDVEC_timer1ovf:  ;{{{  end-of-ISR code
-	; V_scanline++ and store
-	ldi	r19:r18, 1
-	add	r16, r18
-	adc	r17, r19
-	sts	V_scanline_h, r17
-	sts	V_scanline_l, r16
-
-	pop	r16
-	out	SREG, r16
-	pop	ZH
-	pop	ZL
-	pop	r19
-	pop	r18
-	pop	r17
-	pop	r16
-	reti
-
-;}}}
-
-;{{{  interrupt for TIMER1 comparator C
-VEC_timer1compc:
+VEC_timer1compc: ;{{{  interrupt for TIMER1 comparator C
 	; can use r2 as temporary
 	in	r2, SREG
 	push	r16
+	push	r17
 
 	; this is triggered shortly after the timer interrupt on active lines only to kick-off the USART
+	ldi	r16, 0xc0		; MSPIM mode
+	sts	UCSR1C, r16
+	ldi	r16, 0x60		; TXC1, UDRE1
+	sts	UCSR1A, r16
+	ldi	r16, 0x68		; UDRIE, TXCIE, TXEN1
+	sts	UCSR1B, r16
+	ldi	r17:r16, 2		; baud rate
+	sts	UBRR1H, r17
+	sts	UBRR1L, r16
+	; transmitter up-and-running, UDRE interrupt will be ready to fire
 
+	ldi	r16, HRES		; this many bytes please
+	mov	r3, r16
+
+	pop	r17
 	pop	r16
 	out	SREG, r2
 	reti
@@ -379,8 +195,8 @@ VEC_timer1compc:
 
 ;}}}
 VEC_usart1udre:	;{{{  interrupt for USART1 data register empty
-	push	r16
-	in	r16, SREG
+	; can use r2 as temporary
+	in	r2, SREG
 	push	r16
 
 	; more to go
@@ -390,35 +206,38 @@ VEC_usart1udre:	;{{{  interrupt for USART1 data register empty
 	dec	r3
 	brne	1f
 
+	; here means we've just dispatched what is the last thing into the mix, so fixup Y for next line of data (rather, rewind for same data)
+	; also means we've got plenty of time
 	ldi	r16, 0x40		; UDRE interrupt off, disable transmitter (TX-complete interrupt still enabled).
 	sts	UCSR1B, r16
+
+	mov	r16, r5			; r5 is active line [191 -> 0]
+	andi	r16, 0x01
+	breq	1f			; branch if bit 0 is clear
+	sbiw	Y, HRES			; rewind please
 .L1:
 
 	pop	r16
-	out	SREG, r16
-	pop	r16
+	out	SREG, r2
 	reti
 
 ;}}}
 VEC_usart1tx:	;{{{  interrupt for USART1 TX complete
+	; Note: this gets called once at the end of each active line, just as the last byte has been shifted out (to the display)
+
+	; can use r2 as temporary
+	in	r2, SREG
 	push	r16
-	in	r16, SREG
-	push	r16
-	push	r17
+
 
 	ldi	r16, 0x00		; interrupt and transmit disable
 	sts	UCSR1B, r16
-	nop
-	nop
-	nop
 	sts	UBRR1H, r16		; set baud rate to zero
 	sts	UBRR1L, r16
 	cbi	PORTD, 3		; PD3 (TXD1) low
 
-	pop	r17
 	pop	r16
-	out	SREG, r16
-	pop	r16
+	out	SREG, r2
 	reti
 ;}}}
 
@@ -451,7 +270,7 @@ vidhw_setup: ;{{{  sets up video generation stuff
 	sts	PRR1, r16
 
 	; setup USART1-SPI hardware
-	;sbi	DDRD, 3			; PD3/TXD1 (18) as output
+	;sbi	DDRD, 3			; PD3/TXD1 (18) as output	: XXX: set as above (VID_DDR)
 	cbi	DDRD, 2			; PD2/RXD1 (19) as input  [unused]
 	sbi	DDRD, 5			; PD5/XCK1 as output  [not routed]
 
@@ -459,12 +278,11 @@ vidhw_setup: ;{{{  sets up video generation stuff
 
 	clr	r16
 	mov	r3, r16
-	sts	V_xxmask, r16
 
 	ldi	r17:r16, 0		; baud
 	sts	UBRR1H, r17
 	sts	UBRR1L, r16
-	; NOTE: USART transmitter setup and enabled at line start point.
+	; NOTE: USART transmitter setup and enabled at line start point (in response to OCR1C).
 
 	; ICR1 = PAL_CYC_SCANLINE
 	ldi	r17:r16, PAL_CYC_SCANLINE
@@ -481,32 +299,34 @@ vidhw_setup: ;{{{  sets up video generation stuff
 	sts	OCR1BH, r17
 	sts	OCR1BL, r16
 
+	; OCR1C = PAL_CYC_OUTPUT_START
+	ldi	r17:r16, PAL_CYC_OUTPUT_START
+	sts	OCR1CH, r17
+	sts	OCR1CL, r16
+
 	; clear timer1 value
 	clr	r16
 	sts	TCNT1H, r16
 	sts	TCNT1L, r16
 
-	; set scanline and vline-address
-	ldi	r17:r16, (PAL_LINE_FRAME + 1)
-	sts	V_scanline_h, r17
-	sts	V_scanline_l, r16
-
-	ldi	r17, hi(PR_vsync >> 1)
-	ldi	r16, lo(PR_vsync >> 1)
-	sts	A_vline_h, r17
-	sts	A_vline_l, r16
+	; setup location so next interrupt starts a vertical sync
+	ldi	r16, 3
+	mov	r4, r16
+	ldi	r16, 0
+	mov	r5, r16
 
 	; setup TIMER1 for fast PWM mode, TOP=ICR1(=PAL_CYC_SCANLINE); OCR1A update at BOTTOM; overflow interrupt at TOP.
 	; attach OC1A inverted mode, OC1B inverted mode
 	ldi	r16, (TCCR1A_BIT_COM1A1 | TCCR1A_BIT_COM1A0 | TCCR1A_BIT_COM1B1 | TCCR1A_BIT_COM1B0 | TCCR1A_BIT_WGM11)
+	; ldi	r16, (TCCR1A_BIT_COM1A1 | TCCR1A_BIT_COM1A0 | TCCR1A_BIT_WGM11)
 	sts	TCCR1A, r16
 	ldi	r16, (TCCR1B_BIT_WGM13 | TCCR1B_BIT_WGM12 | TCCR1B_BIT_CS10)
 	sts	TCCR1B, r16
 
 	; at this point the timer is running
 
-	; enable timer interrupts: overflow and compare-match-C
-	ldi	r16, TIMSK1_BIT_TOIE1 | TIMSK1_BIT_OCIE1C
+	; enable timer interrupts: overflow
+	ldi	r16, TIMSK1_BIT_TOIE1
 	sts	TIMSK1, r16
 
 	pop	r17
@@ -737,59 +557,111 @@ fb_setbyte: ;{{{  write the 8-bits in r18 to the framebuffer at position (r16*8,
 
 
 ;}}}
-fb_writechar_8x8: ;{{{  writes the ASCII char in r18 to the framebuffer at position (r16*8, r17) using 8x8 font;  updates r16
+fb_writechar: ;{{{  writes the ASCII char in r18 to the framebuffer at position (r16, r17) using 5x7 font.
+
+	; check for valid character (obviously :))
+	cpi	r18, 32
+	brsh	0f
+	rjmp	fb_writechar_out
+.L0:
+	cpi	r18, 128
+	brlo	0f
+	rjmp	fb_writechar_out
+.L0:
+	push	r0
+	push	r1
+	push	r18			; save regs from here
+	push	r19
+	push	r20
+	push	r21
+	push	XL
+	push	XH
 	push	ZL
 	push	ZH
 
+	subi	r18, 32			; normalise character to [0-95]
+
+	; point Z at start of the correct character
+	ldi	ZH, hi(font_table_5x7)
+	ldi	ZL, lo(font_table_5x7)
+	ldi	r19, 8			; bytes per character
+	mul	r18, r19		; result left in r1:r0
+	add	ZL, r0
+	adc	ZH, r1
+
+	; point X at the first byte in the framebuffer of interest
+	ldi	XH, hi(V_framebuffer)
+	ldi	XL, lo(V_framebuffer)
+	ldi	r19, HRES		; bytes per line
+	mul	r17, r19		; Y*HRES in r1:r0
+	add	XL, r0
+	adc	XH, r1
+	mov	r19, r16		; copy of X
+	lsr	r19			; divide by 8 to get byte portion
+	lsr	r19
+	lsr	r19
+	clr	r20
+	add	XL, r19			; addinto X
+	adc	XH, r20
+
+	; FIXME: incomplete!
+
+	mov	r19, r16		;
+	andi	r19, 0x07		;
+	brne	0f
+	rjmp	fb_writechar_offs0
+.L0:
+	dec	r19
+
+fb_writechar_cont:
 
 	pop	ZH
 	pop	ZL
+	pop	XH
+	pop	XL
+	pop	r21
+	pop	r20
+	pop	r19
+	pop	r18
+	pop	r1
+	pop	r0
+fb_writechar_out:
 	ret
 
+fb_writechar_offs0:
+	rjmp	fb_writechar_cont
 
 ;}}}
 
 vid_waitbot: ;{{{  waits for the render to reach bottom of visible area (so we can start updating things)
 	push	r16
-	push	r17
-	push	r18
-	push	r19
 
-	ldi	r19:r18, DPY_STOP_RENDER
 .L0:
-	lds	r17, V_scanline_h
-	lds	r16, V_scanline_l
-	cp	r16, r18
-	cpc	r17, r19
-	breq	1f
-	rjmp	0b
-.L1:
+	mov	r16, r4			; basically wait for (r4 == 1) and (r5 == (PAL_BBLANK_LINES-1)).
+	cpi	r16, 1
+	brne	0b			; nope, wait some more
+	mov	r16, r5
+	cpi	r16, PAL_BBLANK_LINES-1
+	brne	0b			; nope, wait some more (will be waiting a long time here..)
 
-	pop	r19
-	pop	r18
-	pop	r17
+	; yep, at start of bottom blanking region.
+	; this gives us basically 120 PAL lines worth of time before the next frame is drawn <= 7.68ms ~= 122880 cycles at 16 MHz --- MANY LOTS :-)
 	pop	r16
 	ret
 ;}}}
 vid_waitbot1: ;{{{  waits for the render to reach just after bottom of visible area (forces delay with the above)
 	push	r16
-	push	r17
-	push	r18
-	push	r19
 
-	ldi	r19:r18, DPY_STOP_RENDER+1
 .L0:
-	lds	r17, V_scanline_h
-	lds	r16, V_scanline_l
-	cp	r16, r18
-	cpc	r17, r19
-	breq	1f
-	rjmp	0b
-.L1:
+	mov	r16, r4			; basically wait for (r4 == 1) and (r5 == (PAL_BBLANK_LINES-2)).
+	cpi	r16, 1
+	brne	0b			; nope, wait some more
+	mov	r16, r5
+	cpi	r16, PAL_BBLANK_LINES-2
+	brne	0b			; nope, wait some more (will be waiting a long time here..)
 
-	pop	r19
-	pop	r18
-	pop	r17
+	; yep, in 2nd line of bottom blanking region.
+	; this gives us basically 119 PAL lines worth of time before the next frame is drawn <= 7.616ms ~= 121856 cycles at 16 MHz --- MANY LOTS :-)
 	pop	r16
 	ret
 
@@ -877,3 +749,11 @@ loop:
 	nop
 	nop
 	rjmp	loop
+
+
+;------------------------------------------------------------------------------
+; fonts and suchlike
+;------------------------------------------------------------------------------
+
+.include "fb-font5x7.inc"
+
