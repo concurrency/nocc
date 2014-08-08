619f149cdd32cd110d847c231802c3d2864e2c43
nuked <F.R.M.Barnes@kent.ac.uk>
2014-08-08 10:54:42 +0100

    Various commits, AVR/PAL working a bit better now.

--- tests/test_avr33.asm
+++ tests/test_avr33.asm
@@ -11,6 +11,8 @@
 ; Source: a lot of the initial how-to I got from the Arduino TVout library [Myles Metzer, 2010].
 ; Source: assorted inspiration / code observation from 'craft' [Linus Åkesson, 2008].
 
+; MORE GROT: the first handful of X pixels don't get shown for whatever reason (maybe just my display though).
+
 ;{{{  reserved registers (and calling convention)
 
 .def	UDRE_ISR_TMP		=r2		; for the UDRE interrupt routine: this needs to fire every 48 cycles or so to keep up when rendering
@@ -36,8 +38,8 @@
 .equ	CYC_H_SYNC	=74		; (74.2)
 
 .equ	PAL_CYC_SCANLINE	=1023	; 64us lines
-.equ	PAL_CYC_OUTPUT_START	=80
-.equ	PAL_CYC_VGATE_START	=195	; turn on here (OC1B)
+.equ	PAL_CYC_OUTPUT_START	=90
+.equ	PAL_CYC_VGATE_START	=150	; turn on here (OC1B)
 
 ; NOTE: r4 determines vertical region (0=active, 1=bblank, 2=vsync, 3=tblank),
 ;       r5 is the particular line in that region.
@@ -69,8 +71,12 @@ V_framebuffer: ;{{{  suitably sized b&w framebuffer
 	.space	(HRES * VRES)
 
 ;}}}
-;{{{  video variables
-; Meh, all gone!
+;{{{  other variables
+V_scroll1:
+V_scroll1_h:
+	.space	1				; 16-bit organised as 13-bit:3-bit
+V_scroll1_l:
+	.space	1
 
 ;}}}
 
@@ -109,7 +115,8 @@ VEC_timer1ovf:	;{{{  interrupt for TIMER1 overflow
 	ldi	r16, TIMSK1_BIT_TOIE1		;								[17]
 	sts	TIMSK1, r16			; turn off OC1C interrupt					[19]
 	ldi	r16, TIFR1_BIT_OCF1C		;								[20]
-	sts	TIFR1, r16			; clear any pending OC1C interrupt				[22]
+	out	TIFR1, r16			; clear any pending OC1C interrupt				[22]
+	;sts	TIFR1, r16
 
 	rjmp	9f				;								[24]
 
@@ -121,7 +128,6 @@ VEC_timer1ovf:	;{{{  interrupt for TIMER1 overflow
 	ldi	r17:r16, CYC_V_SYNC		;								[19]
 	sts	OCR1AH, r17			;								[21]
 	sts	OCR1AL, r16			;								[23]
-	sbi	PORTB, 7	; DEBUG
 
 	rjmp	9f				;								[25]
 
@@ -133,7 +139,6 @@ VEC_timer1ovf:	;{{{  interrupt for TIMER1 overflow
 	ldi	r17:r16, CYC_H_SYNC		;								[21]
 	sts	OCR1AH, r17			;								[23]
 	sts	OCR1AL, r16			;								[25]
-	cbi	PORTB, 7	; DEBUG
 
 	rjmp	9f				;								[27]
 
@@ -144,12 +149,15 @@ VEC_timer1ovf:	;{{{  interrupt for TIMER1 overflow
 
 .L6:	; first active line [191]										-> [21]
 	; turn on OC1C and its interrupt
+	sbi	PORTB, 7	; DEBUG
 	ldi	YH, hi(V_framebuffer)		;								[22]
 	ldi	YL, lo(V_framebuffer)		;								[23]
 	ldi	r16, TIFR1_BIT_OCF1C		;								[24]
-	sts	TIFR1, r16			; clear any past OC1C interrupt					[26]
+	out	TIFR1, r16			; clear any past OC1C interrupt					[26]
+	;sts	TIFR1, r16
 	ldi	r16, (TIMSK1_BIT_OCIE1C | TIMSK1_BIT_TOIE1)	;						[27]
 	sts	TIMSK1, r16			; enable OC1C and overflow interrupts				[29]
+	cbi	PORTB, 7	; DEBUG
 
 
 .L7:	; in active line [191-0]										-> [14, 29]
@@ -176,19 +184,24 @@ VEC_timer1compc: ;{{{  interrupt for TIMER1 comparator C
 	push	r17
 
 	; this is triggered shortly after the timer interrupt on active lines only to kick-off the USART
-	ldi	r16, 0xc0		; MSPIM mode
-	sts	UCSR1C, r16
-	ldi	r16, 0x60		; TXC1, UDRE1
-	sts	UCSR1A, r16
-	ldi	r16, 0x68		; UDRIE, TXCIE, TXEN1
-	sts	UCSR1B, r16
-	ldi	r17:r16, 2		; baud rate
+	ldi	r17:r16, 0						; baud rate
 	sts	UBRR1H, r17
 	sts	UBRR1L, r16
-	; transmitter up-and-running, UDRE interrupt will be ready to fire
 
-	ldi	r16, HRES		; this many bytes please
+	ldi	r16, (UCSR1A_BIT_TXC1 | UCSR1A_BIT_UDRE1)
+	sts	UCSR1A, r16						; clear these interrupt flags
+	ldi	r16, (UCSR1B_BIT_UDRIE1 | UCSR1B_BIT_TXEN1)
+	sts	UCSR1B, r16
+	; transmitter up-and-running, UDRE interrupt will be ready to fire
+	ldi	r17:r16, 2						; baud rate
+	sts	UBRR1H, r17
+	sts	UBRR1L, r16
+
+	ldi	r16, HRES		; this many bytes more please
 	mov	r3, r16
+
+	;ld	r16, Y+			; first character to go out
+	;sts	UDR1, r16		; some zeros first -- padding
 
 	pop	r17
 	pop	r16
@@ -211,13 +224,13 @@ VEC_usart1udre:	;{{{  interrupt for USART1 data register empty
 
 	; here means we've just dispatched what is the last thing into the mix, so fixup Y for next line of data (rather, rewind for same data)
 	; also means we've got plenty of time
-	ldi	r16, 0x40		; UDRE interrupt off, disable transmitter (TX-complete interrupt still enabled).
+	ldi	r16, UCSR1B_BIT_TXCIE1		; UDRE interrupt off, disable transmitter, TX-complete interrupt enable.
 	sts	UCSR1B, r16
 
 	mov	r16, r5			; r5 is active line [191 -> 0]
 	andi	r16, 0x01
-	breq	1f			; branch if bit 0 is clear
-	sbiw	Y, HRES			; rewind please
+	breq	1f			; branch if bit 0 is clear (2nd, 4th, etc.)
+	sbiw	YH:YL, HRES		; rewind please
 .L1:
 
 	pop	r16
@@ -235,8 +248,8 @@ VEC_usart1tx:	;{{{  interrupt for USART1 TX complete
 
 	ldi	r16, 0x00		; interrupt and transmit disable
 	sts	UCSR1B, r16
-	sts	UBRR1H, r16		; set baud rate to zero
-	sts	UBRR1L, r16
+	;sts	UBRR1H, r16		; set baud rate to zero
+	;sts	UBRR1L, r16
 	cbi	PORTD, 3		; PD3 (TXD1) low
 
 	pop	r16
@@ -285,6 +298,12 @@ vidhw_setup: ;{{{  sets up video generation stuff
 	ldi	r17:r16, 0		; baud
 	sts	UBRR1H, r17
 	sts	UBRR1L, r16
+
+	ldi	r16, (UCSR1C_BIT_UMSEL11 | UCSR1C_BIT_UMSEL10)		; MSPIM mode
+	sts	UCSR1C, r16
+	ldi	r16, 0							; all interrupts off, receiver and transmitter disabled
+	sts	UCSR1B, r16
+
 	; NOTE: USART transmitter setup and enabled at line start point (in response to OCR1C).
 
 	; ICR1 = PAL_CYC_SCANLINE
@@ -599,6 +618,59 @@ fb_writechar: ;{{{  writes the ASCII char in r18 to the framebuffer at position 
 	mul	r17, r19		; Y*HRES in r1:r0
 	add	XL, r0
 	adc	XH, r1
+
+	tst	r16			; see if X is negative (or >= 128)
+	brpl	1f			; nope, straightforward
+
+	cpi	r16, 0xfc		; -4
+	brne	0f
+	rjmp	fb_writechar_offs4r
+.L0:
+	cpi	r16, 0xfd		; -3
+	brne	0f
+	rjmp	fb_writechar_offs5r
+.L0:
+	cpi	r16, 0xfe		; -2
+	brne	0f
+	rjmp	fb_writechar_offs6r
+.L0:
+	cpi	r16, 0xff		; -1
+	brne	0f
+	rjmp	fb_writechar_offs7r
+.L0:
+	; must be off-screen then!
+	rjmp	fb_writechar_cont
+
+.L1:
+	cpi	r16, 124		; see if we're in the right-hand edge region (chars starting 124 -> 127)
+	brlo	1f			; nope, straightforward
+
+	; if we go anywhere from here, must be right-most byte, X+(HRES-1)
+	ldi	r19, HRES-1
+	clr	r20
+	add	XL, r19
+	adc	XH, r20
+
+	cpi	r16, 124
+	brne	0f
+	rjmp	fb_writechar_offs4l
+.L0:
+	cpi	r16, 125
+	brne	0f
+	rjmp	fb_writechar_offs5l
+.L0:
+	cpi	r16, 126
+	brne	0f
+	rjmp	fb_writechar_offs6l
+.L0:
+	cpi	r16, 127
+	brne	0f
+	rjmp	fb_writechar_offs7l
+.L0:
+	; should not get here, ever!
+	rjmp	fb_writechar_cont
+
+.L1:
 	mov	r19, r16		; copy of X
 	lsr	r19			; divide by 8 to get byte portion
 	lsr	r19
@@ -609,12 +681,38 @@ fb_writechar: ;{{{  writes the ASCII char in r18 to the framebuffer at position 
 
 	mov	r19, r16
 	andi	r19, 0x07		; bit start within byte
+	cpi	r19, 0
+	brne	0f
+	rjmp	fb_writechar_offs0
+.L0:
+	cpi	r19, 1
+	brne	0f
+	rjmp	fb_writechar_offs1
+.L0:
+	cpi	r19, 2
+	brne	0f
+	rjmp	fb_writechar_offs2
+.L0:
 	cpi	r19, 3
 	brne	0f
 	rjmp	fb_writechar_offs3	; simple case :)
 .L0:
 
-	; FIXME: incomplete!
+	; then the more complex split-byte cases
+	cpi	r19, 4
+	brne	0f
+	rjmp	fb_writechar_offs4
+.L0:
+	cpi	r19, 5
+	brne	0f
+	rjmp	fb_writechar_offs5
+.L0:
+	cpi	r19, 6
+	brne	0f
+	rjmp	fb_writechar_offs6
+.L0:
+	rjmp	fb_writechar_offs7
+
 
 fb_writechar_cont:
 
@@ -631,10 +729,57 @@ fb_writechar_cont:
 fb_writechar_out:
 	ret
 
-fb_writechar_offs3:
+	; helpers for particular offsets:
+	;
 	; XH:XL = address of byte in FB
 	; ZH:ZL = address of first character byte (5 LSBs)
-	; r16,r17 = pos; r18 = char;  r19-r21 = available.
+	; r16,r17 = pos; r18 = char;  r19-r21,r0-r1 = available.
+fb_writechar_offs0:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	ld	r20, X
+	lpm	r19, Z+
+	lsl	r19			; shift left 3 places
+	lsl	r19
+	lsl	r19
+	or	r20, r19		; OR character data into FB byte
+	st	X, r20
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs1:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	ld	r20, X
+	lpm	r19, Z+
+	lsl	r19			; shift left 2 places
+	lsl	r19
+	or	r20, r19		; OR character data into FB byte
+	st	X, r20
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs2:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	ld	r20, X
+	lpm	r19, Z+
+	lsl	r19			; shift left 1 place
+	or	r20, r19		; OR character data into FB byte
+	st	X, r20
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs3:
 	ldi	r21, 7			; this many bytes please
 .L0:
 	ld	r20, X
@@ -647,6 +792,398 @@ fb_writechar_offs3:
 
 	rjmp	fb_writechar_cont
 
+fb_writechar_offs4:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	clr	r1
+	clc
+	ror	r19			; keep 4 MSB in r19
+	ror	r1			; put 1 LSB into r1 MSB
+
+	ld	r20, X
+	or	r20, r19
+	st	X+, r20			; write left-hand bit, move to next
+
+	ld	r20, X
+	or	r20, r1
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES-1		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs4l:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	lsr	r19			; keep 4 MSB in r19
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write left-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs4r:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	ror	r19
+	ror	r19			; put 1 LSB in r19 MSB
+	andi	r19, 0x80		; cull the rest
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs5:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	clr	r1
+	clc
+	ror	r19			; keep 4 MSB in r19
+	ror	r1			; put 1 LSB into r1 MSB
+
+	ror	r19			; keep 3 MSB in r19
+	ror	r1			; put 2 LSB into tr1 MSB
+
+	ld	r20, X
+	or	r20, r19
+	st	X+, r20			; write left-hand bit, move to next
+
+	ld	r20, X
+	or	r20, r1
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES-1		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs5l:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	lsr	r19			; keep 3 MSB in r19
+	lsr	r19
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write left-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs5r:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	ror	r19
+	ror	r19
+	ror	r19			; put 2 LSB in r19 MSB
+	andi	r19, 0xc0		; cull the rest
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs6:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	clr	r1
+	clc
+	ror	r19			; keep 4 MSB in r19
+	ror	r1			; put 1 LSB into r1 MSB
+
+	ror	r19			; keep 3 MSB in r19
+	ror	r1			; put 2 LSB into r1 MSB
+
+	ror	r19			; keep 2 MSB in r19
+	ror	r1			; put 3 LSB into r1 MSB
+
+	ld	r20, X
+	or	r20, r19
+	st	X+, r20			; write left-hand bit, move to next
+
+	ld	r20, X
+	or	r20, r1
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES-1		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs6l:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	lsr	r19			; keep 2 MSB in r19
+	lsr	r19
+	lsr	r19
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write left-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs6r:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	ror	r19
+	ror	r19
+	ror	r19
+	ror	r19			; put 3 LSB in r19 MSB
+	andi	r19, 0xe0		; cull the rest
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs7:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	clr	r1
+	clc
+	ror	r19			; keep 4 MSB in r19
+	ror	r1			; put 1 LSB into r1 MSB
+
+	ror	r19			; keep 3 MSB in r19
+	ror	r1			; put 2 LSB into r1 MSB
+
+	ror	r19			; keep 2 MSB in r19
+	ror	r1			; put 3 LSB into r1 MSB
+
+	ror	r19			; keep 1 MSB in r19
+	ror	r1			; put 4 LSB into r1 MSB
+
+	ld	r20, X
+	or	r20, r19
+	st	X+, r20			; write left-hand bit, move to next
+
+	ld	r20, X
+	or	r20, r1
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES-1		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs7l:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	lsr	r19			; keep 1 MSB in r19
+	lsr	r19
+	lsr	r19
+	lsr	r19
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write left-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+fb_writechar_offs7r:
+	ldi	r21, 7			; this many bytes please
+.L0:
+	lpm	r19, Z+
+	ror	r19
+	ror	r19
+	ror	r19
+	ror	r19
+	ror	r19			; put 4 LSB in r19 MSB
+	andi	r19, 0xf0		; cull the rest
+
+	ld	r20, X
+	or	r20, r19
+	st	X, r20			; write right-hand bit
+
+	adiw	XH:XL, HRES		; next framebuffer line
+	dec	r21
+	brne	0b
+
+	rjmp	fb_writechar_cont
+
+
+;}}}
+fb_writestring: ;{{{  writes the null-terminated ASCII string pointed at by Z to the framebuffer starting at position (r16, r17) using 5x7 font.
+	push	r16
+	push	r18
+	push	r19
+	push	ZL
+	push	ZH
+
+.L0:
+	lpm	r18, Z+			; load character
+	cpi	r18, 0
+	breq	9f			; done
+
+	rcall	fb_writechar		; write it at the appropriate location
+	subi	r16, (256 - 6)		; add 6
+	brmi	9f			; wrapped off RHS, stop here regardless
+	rjmp	0b			; next character please
+
+.L9:
+	pop	ZH
+	pop	ZL
+	pop	r19
+	pop	r18
+	pop	r16
+	ret
+
+;}}}
+
+fb_pbyteblit: ;{{{  does byte-chunk blitting from program memory to the framebuffer (at r16*8,r17) from Z, width r18*8, height r19
+	push	r0
+	push	r1
+	push	r16
+	push	r17
+	push	r18
+	push	r19
+	push	r20
+	push	r21
+	push	XL
+	push	XH
+	push	ZL
+	push	ZH
+
+	ldi	XH, hi(V_framebuffer)
+	ldi	XL, lo(V_framebuffer)
+	ldi	r20, HRES
+	mul	r17, r20			; r1:r0 = Y * HRES
+	add	XL, r0
+	adc	XH, r1
+	clr	r20
+	add	XL, r16				; add target X (*8)
+	adc	XH, r20
+
+	; X now points at the start in the framebuffer, can re-use r16/r17
+
+	; for each line..
+.L0:
+	mov	r16, r18			; byte count
+
+	mov	r21, XH				; save X (for this line)
+	mov	r20, XL
+.L1:
+	lpm	r17, Z+				; next byte from program memory
+	st	X+, r17				; store in framebuffer
+	dec	r16
+	brne	1b				; more to go
+
+	mov	XH, r21				; restore X
+	mov	XL, r20
+	adiw	XH:XL, HRES			; next line
+
+	dec	r19				; height--
+	brne	0b				; next line please
+
+.L9:
+
+	pop	ZH
+	pop	ZL
+	pop	XH
+	pop	XL
+	pop	r21
+	pop	r20
+	pop	r19
+	pop	r18
+	pop	r17
+	pop	r16
+	pop	r1
+	pop	r0
+	ret
+;}}}
+
+fb_drawline: ;{{{  draws a line starting at (r16,r17) [unsigned] for (r18,r19) [signed]
+	push	r0
+	push	r1
+	push	r16
+	push	r17
+	push	r18
+	push	r19
+	push	r20
+	push	r21
+	push	r24
+	push	r25
+	push	XL
+	push	XH
+
+	cpi	r17, 96
+	brlo	0f			; in range (unsigned)
+	rjmp	9f			; outside raster
+.L0:
+	tst	r16
+	brpl	0f			; is positive
+	rjmp	9f			; outside raster maybe
+.L0:
+	cpi	r18, 0
+	brne	0f
+
+	; FIXME: incomplete!
+	nop
+
+.L0:
+
+.L9:
+	pop	XH
+	pop	XL
+	pop	r25
+	pop	r24
+	pop	r21
+	pop	r20
+	pop	r19
+	pop	r18
+	pop	r17
+	pop	r16
+	pop	r1
+	pop	r0
 ;}}}
 
 vid_waitbot: ;{{{  waits for the render to reach bottom of visible area (so we can start updating things)
@@ -683,6 +1220,24 @@ vid_waitbot1: ;{{{  waits for the render to reach just after bottom of visible a
 
 ;}}}
 
+lvar_init: ;{{{  initialises local stuffs
+	push	r16
+	push	r17
+	push	XL
+	push	XH
+
+	ldi	r17:r16, 0
+	sts	V_scroll1_h, r17
+	sts	V_scroll1_l, r16
+
+	pop	XH
+	pop	XL
+	pop	r17
+	pop	r16
+	ret
+;}}}
+
+
 VEC_reset:
 	cli				; disable interrupts
 	ldi	r16, hi(RAMEND)		; stack-pointer SPH:SPL = RAMEND
@@ -692,6 +1247,7 @@ VEC_reset:
 
 	rcall	fb_pattern
 	rcall	vidhw_setup
+	rcall	lvar_init
 
 	sei				; enable interrupts
 
@@ -712,7 +1268,9 @@ prg_code:
 	ldi	r18, 128
 	rcall	fb_hline
 	ldi	r18, 96
+	ldi	r16, 5
 	rcall	fb_vline
+	ldi	r16, 0
 	ldi	r17, 95
 	ldi	r18, 128
 	rcall	fb_hline
@@ -731,6 +1289,15 @@ prg_code:
 	;rcall	fb_vline
 	;ldi	r16, 11
 	;rcall	fb_vline
+
+	ldi	r16, 4			; X = 32
+	ldi	r17, 9
+	ldi	r18, 11			; W = 88
+	ldi	r19, 86
+	ldi	ZH, hi(I_ada_image)
+	ldi	ZL, lo(I_ada_image)
+	rcall	fb_pbyteblit
+
 
 	; moving portions
 	ldi	r16, 0
@@ -756,15 +1323,66 @@ prg_code:
 .L3:
 
 	; slap in some text
-	ldi	r16, 11
-	ldi	r17, 3
-	ldi	r18, 'f'
-	rcall	fb_writechar
+	lds	r25, V_scroll1_h
+	lds	r24, V_scroll1_l
 
-	ldi	r16, 19
-	ldi	r17, 5
-	ldi	r18, 'b'
-	rcall	fb_writechar
+	; upper 13-bits are message offset, lower 3 are character-offset
+	clr	r16
+	clc
+	ror	r25
+	ror	r24
+	ror	r16
+	ror	r25
+	ror	r24
+	ror	r16
+	ror	r25			; third bit
+	ror	r24
+	ror	r16
+
+	; r25:r24 how has the 13-bit message offset, r16 has 3 MSBs with the offset
+	lsr	r16
+	swap	r16			; r16 = 0-5
+
+	ldi	ZH, hi(S_message)
+	ldi	ZL, lo(S_message)
+	add	ZL, r24
+	adc	ZH, r25			; adjust for message offset
+
+	mov	r17, r16
+	ldi	r16, 5
+	sub	r16, r17		; r16 = 5-0, X position
+	ldi	r17, 2			; Y position
+
+	rcall	fb_writestring
+
+	cpi	r16, 0
+	breq	4f
+	; moving within a character, add 1 to low-order bits
+	lds	r25, V_scroll1_h
+	lds	r24, V_scroll1_l
+	adiw	r25:r24, 1
+	sts	V_scroll1_h, r25
+	sts	V_scroll1_l, r24
+	rjmp	5f
+.L4:
+	; end of this character, onto next
+	lds	r25, V_scroll1_h
+	lds	r24, V_scroll1_l
+	adiw	r25:r24, 8
+	andi	r24, 0xf8		; clear out low-order bits
+	sts	V_scroll1_h, r25
+	sts	V_scroll1_l, r24
+
+	; check to see if we're at the end: Z still pointing at what we're about to display, if the null-character, reset
+	lpm	r16, Z
+	tst	r16			; null-terminator?
+	brne	5f			; nope
+
+	clr	r16
+	sts	V_scroll1_h, r16
+	sts	V_scroll1_l, r16
+.L5:
+
 
 	rcall	vid_waitbot
 	rjmp	1b
@@ -783,4 +1401,12 @@ loop:
 ;------------------------------------------------------------------------------
 
 .include "fb-font5x7.inc"
+
+S_message:
+	.const	"                         Welcome to the AVR/PAL thing!  :-)  :-)  :-)  ",
+		"This is currently re-rendering the text for each frame, and everything else, ",
+		"as well as clearing the framebuffer, just to get an idea of how much work we ",
+		"can do outside the interrupt handlers..", 0x00
+
+.include "fb-ada.inc"
 
